63ad581ed99dc9d3c76a1c2a92d7693673489254
jaaday <jaaday.ufrn@gmail.com>
2022-09-09 11:18:37 -0300

    Commit inical do projeto PHP - Currículos

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php/app-php',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0165ba2ab05b58226bcbefed57d5b8770c9a0267',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php/app-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0165ba2ab05b58226bcbefed57d5b8770c9a0267',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
